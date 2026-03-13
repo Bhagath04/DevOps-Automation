a5ee902dadc0b4da2cf87a4707f0c5118558caa8
Bhagath04 <126044720+Bhagath04@users.noreply.github.com>
2026-03-13 11:06:31 +0530

    Update rag_engine.py

--- backend/rag_engine.py
+++ backend/rag_engine.py
@@ -4,11 +4,16 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 KB_PATH = os.path.join(BASE_DIR, "knowledge_base")
 
-documents = SimpleDirectoryReader(KB_PATH).load_data()
+documents = SimpleDirectoryReader(
+    KB_PATH,
+    recursive=True,
+    required_exts=[".sh", ".txt", ".md"]
+).load_data()
+
 index = VectorStoreIndex.from_documents(documents)
 
 query_engine = index.as_query_engine()
 
-def query_rag(question: str):
+def query_rag(question):
     response = query_engine.query(question)
     return str(response)
